22ebee1b687d32fdfca3f57b8267586da305cd28
Farid Kurbanou <kurbanou.faridun@gmail.com>
2023-05-12 13:04:53 +0300

    ww

--- skidlesTwo/contacts.php
+++ skidlesTwo/contacts.php
@@ -8,88 +8,128 @@
 
     <main>
         <section class="contact">
-              <div class="contact__intro">
-                <div class="recvizity">
-                    <a href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/doc/rekvizity.pdf" target="_blank"><p class="subtitle">Реквизиты скачать...</p></a>
-                    <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/svg/pdf.svg" width="24" alt="pdf">
+            
+             <div class="contact__intro">
+
+                <div class="contact__intro_inner">
+
+                    <div class="contact_item">
+                        <div class="contact_item_icon"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/svg/@.svg" alt="icon"></div>
+                        <div class="contact_item_text">
+                            <a href="mailto:priemnaja.skidlhz@gmail.com" target="_blank">
+                                <p>priemnaja.skidlhz@gmail.com</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="contact_item">
+                        <div class="contact_item_icon"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/svg/phone.svg" alt="icon"></div>
+                        <div class="contact_item_text">
+                            <a href="tel:+375152473042">
+                                <p>+375 152 47-30-42</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="contact_item">
+                        <div class="contact_item_icon"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/svg/info.svg" alt="icon"></div>
+                        <div class="contact_item_text">
+                            <a href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/doc/rekvizity.pdf" target="_blank">
+                                <p>Наши реквизиты</p>
+                            </a>
+                        </div>
+                    </div>
+
                 </div>
             </div>
+            
+                 <div class="contacts__book_inner">
+                <div class="wrapper">
+                    <div class="contacts__book">
+                        <p class="subtitle">Лесхоз:</p>
+                        <table class="contacts__book_table">
+                            <tr>
+                                <td >Директор <i>Бумбуль Анатолий Чеславович</i></td>
+                                <td><a href="tel:+375152473045">+375152473045</a></td>
+                            </tr>
+                            <tr>
+                                <td>Главный лесничий <i>Лисай Артемий Геннадьевич</td>
+                                <td><a href="tel:+375152473043">+375152473043</a></td>
+                            </tr>
+                            <tr>
+                                <td>Заместитель руководителя организации по идеологической работе  <i>Гриневич Евгений Антонович</td>
+                                <td><a href="tel:+375152473050">+375152473050</a></td>
+                            </tr>
+                            <tr>
+                                <td>Главный инженер <i>Езерский Сергей Сергеевич</td>
+                                <td><a href="tel:+375152473056">+375152473056</a></td>
+                            </tr>
+                            <tr>
+                                <td>Главный бухгалтер <i>Козел Наталья Александровна</td>
+                                <td><a href="tel:+375152472995">+375152472995</a></td>
+                            </tr>
+                            <tr>
+                                <td>Производственный отдел</td>
+                                <td><a href="tel:+375152473049">+375152473049</a></td>
+                            </tr>
+                            <tr>
+                                <td>Лесохозяйственный отдел</td>
+                                <td><a href="tel:+375152473052">+375152473052</a></td>
+                            </tr>
+                            <tr>
+                                <td>Отдел кадров, юрисконсульт</td>
+                                <td><a href="tel:+375152473053">+375152473053</a></td>
+                            </tr>
+                            <tr>
+                                <td>Бюро по охране труда</td>
+                                <td><a href="tel:+375152473041">+375152473041</a></td>
+                            </tr>
+                        </table>
 
+                        <p class="subtitle">Лесничества:</p>
 
-                    <div class="contacts__book_inner">
-                        <div class="wrapper">
-                            <div class="contacts__book">
-                                <p class="subtitle">Лесхоз:</p>
-                                <div class="contacts__book_item">
-                                    <p><span>Директор</span><span><a href="tel:+375152473045">+375152473045</a></span></p>
-                                </div>
-
-                                <div class="contacts__book_item">
-                                    <p><span>Главный лесничий</span><span><a href="tel:+375152473043">+375152473043</a></span></p>
-                                </div>
-
-                                <div class="contacts__book_item">
-                                    <p><span>Заместитель руководителя организации по идеологической работе</span><span><a href="tel:+375152473050">+375152473050</a></span></p>
-                                </div>
-
-                                <div class="contacts__book_item">
-                                    <p><span>Главный инженер</span><span><a href="tel:+375152473056">+375152473056</a></span></p>
-                                </div>
-
-                                <div class="contacts__book_item">
-                                    <p><span>Главный бухгалтер</span><span><a href="tel:+375152472995">+375152472995</a></span></p>
-                                </div>
-
-                                <div class="contacts__book_item">
-                                    <p><span> Производственный отдел</span><span><a href="tel:+375152473049">+375152473049</a></span></p>
-                                </div>
-
-                                <div class="contacts__book_item">
-                                    <p><span>Лесохозяйственный отдел</span><span><a href="tel:+375152473052">+375152473052</a></span></p>
-                                </div>
-
-                                <div class="contacts__book_item">
-                                    <p><span>Отдел кадров, юрисконсульт</span><span><a href="tel:+375152473053">+375152473053</a></span></p>
-                                </div>
-
-                                <div class="contacts__book_item">
-                                    <p><span>Бюро по охране труда</span><span><a href="tel:+375152473041">+375152473041</a></span></p>
-                                </div>
-                            </div>
-
-
-                            <div class="contacts__book">
-                                <p class="subtitle">Лесничества:</p>
-                                <div class="contacts__book_item">
-                                    <p><span>Берштовское лесничество</span><span><a href="tel:+375151474127">+375151474127</a></span></p>
-                                </div>
-                                <div class="contacts__book_item">
-                                    <p><span>Скидельское лесничество</span><span><a href="tel:+375152963120">+375152963120</a></span></p>
-                                </div>
-                                <div class="contacts__book_item">
-                                    <p><span>Лунненское лесничество</span><span><a href="tel:+375151533894">+375151533894</a></span></p>
-                                </div>
-                                <div class="contacts__book_item">
-                                    <p><span>Новорудское лесничество</span><span><a href="tel:+375152993785">+375152993785</a></span></p>
-                                </div>
-                                <div class="contacts__book_item">
-                                    <p><span>Поречское лесничество</span><span><a href="tel:+375152479707">+375152479707</a></span></p>
-                                </div>
-                                <div class="contacts__book_item">
-                                    <p><span>Озерское лесничество</span><span><a href="tel:+375152479709">+375152479709</a></span></p>
-                                </div>
-                                <div class="contacts__book_item">
-                                    <p><span>Лесопункт озеры</span><span><a href="tel:+375152479708">+375152479708</a></span></p>
-                                </div>
-                            </div>
-
-                        </div>
-
-
-
-
-
+                        <table class="contacts__book_table">                            
+                            <tr>
+                                <td>Берштовское лесничество</td>
+                                <td><a href="tel:+375151474127">+375151474127</a></td>
+                            </tr>
+                            <tr>
+                                <td>Скидельское лесничество</td>
+                                <td><a href="tel:+375152963120">+375152963120</a></td>
+                            </tr>
+                            <tr>
+                                <td>Лунненское лесничество</td>
+                                <td><a href="tel:+375151533894">+375151533894</a></td>
+                            </tr>
+                            <tr>
+                                <td>Новорудское лесничество</td>
+                                <td><a href="tel:+375152993785">+375152993785</a></td>
+                            </tr>
+                            <tr>
+                                <td>Поречское лесничество</td>
+                                <td><a href="tel:+375152479707">+375152479707</a></td>
+                            </tr>
+                            <tr>
+                                <td>Озерское лесничество</td>
+                                <td><a href="tel:+375152479709">+375152479709</a></td>
+                            </tr>
+                            <tr>
+                                <td>Лесопункт озеры</td>
+                                <td><a href="tel:+375152479708">+375152479708</a></td>
+                            </tr>
+                        </table>   
+                     
                     </div>
+
+                </div>
+
+
+
+
+
+            </div>
+           
+
+
+                    
 
             </div>
 
