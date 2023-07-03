73b17b7c72a476128da7f9efe155f7a318a1abb7
Farid Kurbanou <kurbanou.faridun@gmail.com>
2023-07-03 22:14:42 +0300

    imageBox add

--- pages/post/script.js
+++ pages/post/script.js
@@ -1,8 +1,23 @@
 'use strict'
 
 import { nav as nav } from "../../assets/js/nav.js";
-// import { search as search } from "../../assets/js/search.js";
-
+import { popupCreate } from "../../assets/js/popup.js";
 
 nav();
-// search();
+
+const content = document.querySelector('.content');
+
+if(content && window.screen.width > 1000){
+    const images = content.querySelectorAll('img');
+    images.forEach(image => {
+        image.addEventListener('click', () => {
+            popupCreate();
+            const popupInner = document.querySelector('.popup_inner');
+            const imageBox = document.createElement('div');
+            imageBox.classList = 'imageBox';
+            popupInner.appendChild(imageBox);
+            imageBox.innerHTML =`${image.outerHTML}`
+
+        })}
+    )
+}
